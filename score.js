5c88f343d0256429a950f77a3cf12a4157e24bfa
Jad Khalili <jad@videolab.ae>
2022-01-30 09:45:38 -0600

    Speed scoring

--- score.js
+++ score.js
@@ -54,10 +54,56 @@ function getBrakeSpikes(detectionValues) {
 }
 
 
+function parseSpeedFile(id) {
+    data = fs.readFileSync("./data/speed_" + id + ".csv").toString();
+    data = data.split("\n").slice(1);
+    d = []
+    for(var i = 0; i < data.length; i++) {
+        d.push(parseFloat(data[i],10))
+    }
+
+    return d;
+}
+function getSpeedViolations(s) {
+    console.log(s)
+    var violations = 0
+    var i = 0;
+    while(i < s.length-BATCH_SIZE) {
+        var avg = 0;
+
+        for(var j = i; j <= i+BATCH_SIZE; j++) {
+            avg += Math.abs(s[j]);
+        }
+        avg /= BATCH_SIZE;
+
+        for(var j = i; j <= i+BATCH_SIZE; j++) {
+            if(Math.abs(s[j]) > avg) {
+                violations++;
+            }
+        }
+        i += BATCH_SIZE;
+    }
+
+    var avg = 0;
+
+    for(var j = i; j <= i+BATCH_SIZE; j++) {
+        avg += Math.abs(s[j]);
+    }
+    avg /= BATCH_SIZE;
+
+    for(var j = i; j <= i+BATCH_SIZE; j++) {
+        if(Math.abs(s[j]) > avg) {
+            violations++;
+        }
+    }
+
+    return violations;
+}
 
 function score(id) {
     var out = {};
 }
 
-console.log(getBrakeSpikes(parseDetectionFile('1e19fa82-a064-4fdb-af6c-63f4e8a2069d')))
+// console.log(getBrakeSpikes(parseDetectionFile('1e19fa82-a064-4fdb-af6c-63f4e8a2069d')))
+console.log(getSpeedViolations(parseSpeedFile('eee368be-dbce-4feb-af5f-709dedc20498')))
 // export default score;
