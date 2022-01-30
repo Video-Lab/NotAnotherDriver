8643d8c5b9d4b23a157de83f56d99965022c7981
Jad Khalili <jad@videolab.ae>
2022-01-30 10:21:39 -0600

    Scoring algorithm

--- score.js
+++ score.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 BATCH_SIZE = 10; // How many time periods to check in average for lane detection
-BRAKE_THRESHOLD = 1000
+BRAKE_THRESHOLD = 600;
 
 function parseFile(id, type) {
     data = fs.readFileSync("./data/" + type + "_" + id + ".csv").toString();
@@ -26,7 +26,6 @@ function getBrakeSpikes(s) {
     var inBrake = false;
     var brakes = 0;
 
-    console.log(s)
     for(var i = 0; i < s.length; i++) {
         if(Math.abs(s[i]) > BRAKE_THRESHOLD && !inBrake) {
             inBrake = true;
@@ -42,7 +41,6 @@ function getBrakeSpikes(s) {
 }
 
 function getSpeedViolations(s) {
-    console.log(s)
     var violations = 0
     var i = 0;
     while(i < s.length-BATCH_SIZE) {
@@ -74,13 +72,29 @@ function getSpeedViolations(s) {
         }
     }
 
-    return violations;
+    return violations*5/s.length;
 }
 
 function score(id) {
     var out = {};
+    var brakes = getBrakeSpikes(parseFile(id, "detection"));
+    var speeds = getSpeedViolations(parseFile(id, "speed"));
+
+    var score = (10-10*Math.min(1,(brakes+1)*speeds))
+    out["score"] = score;
+    out["suggestions"] = [];
+    if(brakes > 0) {
+        out["suggestions"].push("brakes")
+    }
+
+    if(speeds > 0.45) {
+        out["suggestions"].push("speed")
+    }
+
+    return out
 }
 
 // console.log(getBrakeSpikes(parseDetectionFile('1e19fa82-a064-4fdb-af6c-63f4e8a2069d')))
-console.log(getBrakeSpikes(parseFile('eee368be-dbce-4feb-af5f-709dedc20498', 'detection')))
+// console.log(getBrakeSpikes(parseFile('eee368be-dbce-4feb-af5f-709dedc20498', 'detection')))
+console.log(score('eee368be-dbce-4feb-af5f-709dedc20498'))
 // export default score;
