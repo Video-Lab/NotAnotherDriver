012df028ac471ac790d1e1d78da6ce4609374d09
Jad Khalili <jad@videolab.ae>
2022-01-30 10:57:09 -0600

    Index page

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var app = express()
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
 
 app.get('/', function(req,res) {
-    res.send('test')
+    res.render('index', {title: 'Home'})
 })
 
-app.listen(3000)
+app.listen(3000);
